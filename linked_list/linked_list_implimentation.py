ee56dfb5beddb814606159675df80d6f79b13221
Richard Pryce <rich_pryce@yahoo.co.uk>
2021-01-11 12:18:36 +0000

    Added append function on linked list

---
+++ linked_list/linked_list_implimentation.py
@@ -0,0 +1,37 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = self.head
+        self.length = 0
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head == None:
+            self.head = new_node
+            self.tail = self.head
+            self.length = 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length += 1
+
+    def print_list(self):
+        if self.head == None:
+            print('Empty')
+        else:
+            current_node = self.head
+            while current_node != None:
+                print(current_node.data, end=' ')
+                current_node = current_node.next
+        print()
+
+
+my_linkedlist = LinkedList()
+my_linkedlist.append(10)
+my_linkedlist.append(11)
+my_linkedlist.print_list()
