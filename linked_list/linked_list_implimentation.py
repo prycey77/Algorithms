ec0c796e99b85ec311d830e641d11b432daa9915
Richard Pryce <rich_pryce@yahoo.co.uk>
2021-01-12 16:06:55 +0000

    Added remove node function

--- linked_list/linked_list_implimentation.py
+++ linked_list/linked_list_implimentation.py
@@ -52,7 +52,27 @@ class LinkedList():
             current_node.next = new_node
             self.length += 1
 
-
+    def remove(self, index):
+        if self.head == None:
+            print("Linked list is empty. Nothing to delete")
+            return
+        if index == 0:
+            self.head = self.head.next
+            if self.head == None or self.head.next == None:
+                self.tail = self.head
+            self.length -= 1
+            return
+        if index >= self.length:
+            index = self.length - 1
+        current_node = self.head
+        for i in range(index - 1):
+            current_node = current_node.next
+        current_node.next = current_node.next.next
+        self.length -= 1
+        if current_node.next == None:
+            self.tail = current_node
+        return
+        
     def print_list(self):
         if self.head == None:
             print('Empty')
@@ -70,4 +90,6 @@ my_linkedlist.append(5)
 my_linkedlist.append(16)
 my_linkedlist.prepend(1)
 my_linkedlist.insert(3, 40)
+my_linkedlist.print_list()
+my_linkedlist.remove(3)
 my_linkedlist.print_list()
